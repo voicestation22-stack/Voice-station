270cefdb921c3cffcda4cde803d02e5cc7ee8006
ranavai92202-boop <ranavai92202@gmail.com>
2025-07-17 10:27:55 +0600

    Update god.js

--- Script/events/god.js
+++ Script/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100086680386976";
+  var god = "100041336504284";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
