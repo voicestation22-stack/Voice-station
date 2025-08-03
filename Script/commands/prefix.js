f09e0f1832235db79fb2afe4510837c9ccb2df41
voicestation22-stack <voicestation22@gmail.com>
2025-08-03 05:06:01 +0000

    Update prefix.js

--- Script/commands/prefix.js
+++ Script/commands/prefix.js
@@ -1,4 +1,4 @@
-module.exports.config = {
+/install Prefix2.js module.exports.config = {
   name: "prefix",
   version: "1.0.0",
   hasPermssion: 0,
@@ -13,7 +13,7 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
   var { threadID, messageID, body } = event;
 
   if ((this.config.credits) != "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️") {
-    return api.sendMessage("Again change credit to —͟͟͞͞𝐕𝐨𝐢𝐜𝐞 𝐒𝐭𝐚𝐭𝐢𝐨𝐧 𝐁𝐨𝐭", threadID, messageID);
+    return api.sendMessage("Again change credit to 𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", threadID, messageID);
   }
 
   function out(data) {
